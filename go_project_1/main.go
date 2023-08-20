44e7780d762bd19dec134face85adc91ead5dee5
Sandra Ashipala <sandraashipala@gmail.com>
2023-08-20 17:40:07 +0200

    learned how to find variable type

--- go_project_1/main.go
+++ go_project_1/main.go
@@ -8,8 +8,17 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = 10
 
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+
 	fmt.Printf("Welcome to %v booking application. ", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your ticket here to attend")
+
+	var userName string
+	var userTicket int
+	// ask user for their name
+	userName = "Tom"
+	userTicket = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTicket)
 
 }
