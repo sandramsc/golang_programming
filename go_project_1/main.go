599c3f595faae11f61bbc6d28b8f08c4c100f34b
Sandra Ashipala <sandraashipala@gmail.com>
2023-08-20 21:09:37 +0200

    learned how to assign a pointer

--- go_project_1/main.go
+++ go_project_1/main.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go conference"
+	// one can add a colon to a variable, but not a const or a var
+	conferenceName := "Go conference"
 	// const tells Go that the value is not allowed to change
-	const conferenceTickets = 50
-	var remainingTickets = 10
+	const conferenceTickets int = 50
+	var remainingTickets int = 10
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
@@ -14,11 +15,25 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your ticket here to attend")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTicket int
 	// ask user for their name
-	userName = "Tom"
+	// pointer will print out the memory address of the stored variable
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email name: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTicket)
+
 	userTicket = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTicket)
+	fmt.Printf("Thank you, %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTicket, email)
 
 }
